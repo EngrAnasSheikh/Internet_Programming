74f87cf65df05033b9b20f0381a49cda24fb7f17
EngrAnasSheikh <anas_mudassar@yahoo.co.uk>
2021-02-10 16:49:38 +0000

    Week1 & Week2 Commit

---
+++ Week1/java.js
@@ -0,0 +1,15 @@
+function inter() {
+		window.alert('Welcome to Internet Programming Module');
+}
+
+function intel() {
+		window.alert('Welcome to Intelligent Systems Module');
+}
+
+function soft() {
+		window.alert('Welcome to Software Engineering Module');
+}
+
+function anas() {
+		window.alert('Welcome to My Details');
+}
